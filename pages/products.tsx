9e6d659a61b98950f673f74d7dd3ac8aedfcd161
David-49 <dd08.brad@hotmail.fr>
2022-03-29 21:31:11 +0200

    feat: Setup connection to postgres database and add example

---
+++ pages/products.tsx
@@ -0,0 +1,24 @@
+import { GetStaticProps, NextPage } from "next";
+import { pool } from "../utils/database";
+
+interface IProps {
+  dateFormatted: string;
+}
+
+const Products: NextPage<IProps> = (props) => {
+  const { dateFormatted } = props;
+  console.log(dateFormatted);
+
+  return <h1>Hello {dateFormatted}</h1>;
+};
+
+export default Products;
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const query = "SELECT NOW()";
+  const dateNow = (await pool.query(query)).rows;
+  const dateFormatted = dateNow[0].now.toString();
+  return {
+    props: { dateFormatted },
+  };
+};
