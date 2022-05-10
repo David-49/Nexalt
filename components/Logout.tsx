374b9f4869882476fcd7630dab9a5a06a675321d
DavidD <david.dognin@proton.me>
2022-05-11 00:43:29 +0200

    ✨feat(ConnectionActions): Create form to manage connections

---
+++ components/Logout.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import { useAuth } from '../context/AuthContext';
+
+const Logout = () => {
+  const { user, logout } = useAuth();
+  const router = useRouter();
+  const handleLogout = () => {
+    logout();
+    router.push('auth/login');
+  };
+  return (
+    <button disabled={!user} type="submit" onClick={handleLogout}>
+      Me déconnecter
+    </button>
+  );
+};
+
+export default Logout;
