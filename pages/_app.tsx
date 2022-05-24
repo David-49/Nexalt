d11d70c2a9e6c395cc0909bdfefc290b124d23ab
DavidD <david.dognin@proton.me>
2022-05-24 23:14:31 +0200

    feat/Setup the system for layout per page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,27 @@ import '../config/firebase';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { MantineProvider } from '@mantine/core';
+import { ReactNode } from 'react';
+import { NextPage } from 'next';
 import { AuthContextProvider } from '../context/AuthContext';
 import { Logout } from '../components/Authentification/Logout';
 import { ProtectedRoute } from '../components/Authentification/ProtectedRoute';
 import { mantineTheme } from '../theme/index';
 import { GlobalStyles } from '../theme/GlobalStyles';
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout<P = {}> = NextPage<P> & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const noAuthRequired = ['/auth/signup', '/auth/login'];
   const router = useRouter();
+
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
   return (
     <AuthContextProvider>
@@ -20,10 +32,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <GlobalStyles />
         <Logout />
         {noAuthRequired.includes(router.pathname) ? (
-          <Component {...pageProps} />
+          getLayout(<Component {...pageProps} />)
         ) : (
           <ProtectedRoute>
-            <Component {...pageProps} />
+            {getLayout(<Component {...pageProps} />)}
           </ProtectedRoute>
         )}
       </MantineProvider>
