2881c4ff4b4d60e6d7598f3818a008238244bf1e
DavidD <david.dognin@proton.me>
2022-07-09 19:34:04 +0200

    ✨feat(globalStyles.tsx): Add default style to the body

--- theme/globalStyles.tsx
+++ theme/globalStyles.tsx
@@ -1,8 +1,13 @@
 import { Global } from '@mantine/core';
+import { colors } from '.';
 
 export const GlobalStyles = () => (
   <Global
     styles={(theme) => ({
+      body: {
+        backgroundColor: colors.primaryBackgroundColor,
+        overflowX: 'hidden',
+      },
       li: {
         listStyle: 'none',
       },
