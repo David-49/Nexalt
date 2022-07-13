3ef20be51bffd0029649c89aa8f67cb234ccb17e
DavidD <david.dognin@proton.me>
2022-07-14 00:53:23 +0200

    ⚙️refactor(Layout): Create a layout without using Appshell from mantine

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,12 +2,16 @@ import { FC } from 'react';
 
 import { AppShell, createStyles } from '@mantine/core';
 import { Header } from './Header';
+import { colors } from '../theme';
+import { Footer } from './Footer';
 
 interface IProps {}
 
 const useStyles = createStyles((theme) => ({
   containerAppShell: {
     width: '100%',
+    position: 'relative',
+    zIndex: 1,
   },
 }));
 
@@ -18,11 +22,23 @@ export const Layout: FC<IProps> = (props) => {
     <AppShell
       className={classes.containerAppShell}
       header={<Header />}
+      footer={<Footer />}
       padding={0}
       styles={{
         main: { width: '100%' },
       }}
     >
+      <div
+        style={{
+          position: 'absolute',
+          height: 800,
+          bottom: 0,
+          left: 0,
+          width: 200,
+          backgroundColor: colors.secondaryBlue,
+          zIndex: -1,
+        }}
+      />
       {children}
     </AppShell>
   );
