e3133a47401b8ee84ea2cab8a67d7bc657cd030b
Lilian <lilianbeillard@orange.fr>
2022-07-16 22:25:07 +0200

    intégration page blog finie

---
+++ components/BlogPage/BlogCategories.tsx
@@ -0,0 +1,103 @@
+import { FC } from 'react';
+
+import { createStyles, Title } from '@mantine/core';
+import Link from "next/link";
+import { colors } from '../../theme';
+
+interface IProps {}
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: 40,
+    fontWeight: 800,
+    color: '#fff',
+  },
+  subtitle: {
+    fontFamily: 'Poppins, sans-serif',
+    fontWeight: 400,
+    fontSize: 16,
+    color: colors.secondaryBackgroundColor,
+    width: '80%',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginRight: 20,
+
+  },
+  category: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '35%',
+    marginTop: 20,
+    marginRight: 0,
+    marginBottom: 0,
+    marginLeft: 0,
+    padding: 0,
+    '@media (max-width: 1720px)': {
+      width: '50%',
+    },
+  },
+  categoryList: {
+    listStyle: 'none',
+  },
+  categoryBtn: {
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: 20,
+    fontWeight: 700,
+    color: colors.blackText,
+    backgroundColor: colors.secondaryBackgroundColor,
+    paddingTop: 15,
+    paddingRight: 20,
+    paddingBottom: 15,
+    paddingLeft: 20,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    borderRadius: 15,
+  },
+}));
+
+export const BlogCategories: FC<IProps> = (props) => {
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <Title className={classes.title} order={2}>
+          Catégories
+        </Title>
+        <p className={classes.subtitle}>Afin d’avoir une ressources qui colle au mieux à vos recherches, vous pouvez choisir entre les différentes catégories qui se proposent à vous.</p>
+      </div>
+      <ul className={classes.category}>
+        <li className={classes.categoryList}>
+          <Link href="">
+            <a className={classes.categoryBtn}>CV</a>
+          </Link>
+        </li>
+        <li className={classes.categoryList}>
+          <Link href="">
+            <a className={classes.categoryBtn}>Etudiants</a>
+          </Link>
+        </li>
+        <li className={classes.categoryList}>
+          <Link href="">
+            <a className={classes.categoryBtn}>Entreprises</a>
+          </Link>
+        </li>
+        <li className={classes.categoryList}>
+          <Link href="">
+            <a className={classes.categoryBtn}>Guides</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,21 +1,190 @@
-import { ReactElement, ReactNode } from 'react';
-
-import { NextPage } from 'next';
+import { Container, createStyles, Text, Title } from '@mantine/core';
+import Head from 'next/head';
+import type { NextPage } from 'next';
+import Image from "next/image";
+import { colors } from '../theme';
+import {BlogCategories} from "../components/BlogPage/BlogCategories";
+import {BlogCards} from "../components/BlogPage/BlogCards";
+import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
-type HomePageWithLayout = NextPage & {
-  getLayout: (page: ReactElement) => ReactNode;
-};
+interface IProps {}
 
-const Blog: HomePageWithLayout = () => <h1>Hello, c'est la page du blog</h1>;
+const useStyles = createStyles(() => ({
+    containerPage: {
+        padding: 0,
+        width: '100%',
+    },
+    containerHeader: {
+        paddingLeft: '150px',
+        backgroundColor: colors.primaryBackgroundColor,
+        zIndex: 4,
+        position: 'relative',
+        width: '93%',
+        marginRight: 'auto',
+    },
+    containerWhiteBlockTop: {
+        backgroundColor: colors.primaryBackgroundColor,
+        borderBottomRightRadius: 50,
+        paddingBottom: 180,
+        paddingTop: 140,
+        width: '93%',
+        marginRight: 'auto',
+        paddingLeft: '150px',
+        paddingRight: '150px',
+    },
+    title: {
+        fontSize: 50,
+        lineHeight: 1,
+        fontFamily: 'Montserrat',
+        zIndex: 4,
+        position: 'relative',
+        textTransform: 'uppercase',
+    },
+    textIntro: {
+        marginTop: 25,
+        width: '50%',
+        zIndex: 4,
+        position: 'relative',
+        '@media (max-width: 1720px)': {
+            width: '40%',
+        },
+        '@media (max-width: 1580px)': {
+            width: '100%',
+        },
+    },
+    photoGroupContainer: {
+        position: 'absolute',
+        right: 350,
+        bottom: -8,
+        width: 300,
+        zIndex: 0,
+        '@media (max-width: 1580px)': {
+            display: 'none',
+        }
+    },
+    firstBackgroundDecorationContainer: {
+        position: 'absolute',
+        zIndex: -1,
+        right: 0,
+        top: 0,
+    },
+    blueShapeTop: {
+        position: 'absolute',
+        top: -638,
+        bottom: 0,
+        right: 0,
+        width: 200,
+        backgroundColor: colors.primaryBlue,
+        zIndex: -2,
+    },
+    containerStudentsPrices: {
+        backgroundColor: colors.primaryBlue,
+        borderTopLeftRadius: 50,
+        borderBottomLeftRadius: 50,
+        width: '95%',
+        marginLeft: 'auto',
+        padding: 50,
+        paddingLeft: 60,
+    },
+    containerOurMission: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 140,
+        width: '100%',
+        paddingLeft: '150px',
+        paddingRight: '150px',
+    },
+    containerOurVision: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 140,
+        marginBottom: 140,
+        width: '100%',
+        paddingLeft: '150px',
+        paddingRight: '150px',
+    },
+    containerOurTeam: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 140,
+        marginBottom: 140,
+        width: '100%',
+        paddingLeft: '150px',
+        paddingRight: '150px',
+    },
+    containerFooter: {
+        backgroundColor: colors.secondaryBlue,
+        width: '95%',
+        paddingTop: 60,
+        borderTopRightRadius: 50,
+        paddingBottom: 40,
+    },
+}));
 
-Blog.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <>
-      <Header />
-      {page}
-    </>
-  );
+const Blog: NextPage<IProps> = () => {
+    const { classes } = useStyles();
+
+    return (
+        <Container fluid className={classes.containerPage}>
+            <Head>
+                <title>Offres et tarifs</title>
+                <meta name="description" content="NVous pourrez retrouver toutes les offres et tarifs proposés par nexalt" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={classes.containerHeader}>
+                <Header />
+            </div>
+            <main>
+                <div>
+                    <div style={{ position: 'relative' }}>
+                        <div className={classes.blueShapeTop} />
+                        <div className={classes.containerWhiteBlockTop}>
+                            <div className={classes.photoGroupContainer}>
+                                <Image src='/assets/images/blog_picture.png' width={2505} height={4819} priority />
+                            </div>
+                            <Title order={1}>
+                                <Text
+                                    className={classes.title}
+                                    weight={800}
+                                    color={colors.primaryBlue}
+                                >
+                                    Nos
+                                </Text>
+                                <Text
+                                    className={classes.title}
+                                    weight={800}
+                                    color={colors.secondaryBlue}
+                                >
+                                    ressources
+                                </Text>
+                            </Title>
+                            <Text size="xl" className={classes.textIntro}>
+                                Nexalt souhaite vous accompagner dans toutes vos démarches.
+                                Pour ce faire, nous avons une équipe constituée d’experts pouvant vous donner les meilleurs conseils en toute matière !<br/>
+                                Toujours pas convaincu ? Jetez-y un œil, vous nous en redirez des nouvelles…
+                            </Text>
+                        </div>
+                    </div>
+                    <div className={classes.containerStudentsPrices}>
+                        <BlogCategories />
+                    </div>
+                    <div className={classes.containerOurTeam}>
+                        <BlogCards />
+                    </div>
+                </div>
+            </main>
+            <div className={classes.containerFooter}>
+                <Footer />
+            </div>
+        </Container>
+    );
 };
 
 export default Blog;
