4fc58095d20439ba726fa4315996b993d48fa1e4
DavidD <david.dognin@proton.me>
2022-07-17 17:13:16 +0200

    ✨feat(types): Add new types

--- types/Login.d.ts
+++ types/Login.d.ts
@@ -1,4 +1,12 @@
-export interface ILogin {
+export interface ISignup {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirm: string;
+}
+
+export interface ISignin {
   email: string;
   password: string;
 }
